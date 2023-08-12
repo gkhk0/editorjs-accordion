b921004a7f7672d0affbdeb638b059ad1a994d61
Erik Wällstedt <erik.wallstedt@learnet.se>
2023-08-12 23:24:08 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,129 +1,64 @@
 /**
- * Import Tool's icon
- */
-import { IconWarning } from '@codexteam/icons';
-
-/**
  * Build styles
  */
-require('./index.css').toString();
+require("./index.css").toString();
 
-/**
- * @class Warning
- * @classdesc Warning Tool for Editor.js
- * @property {WarningData} data - Warning Tool`s input and output data
- * @property {object} api - Editor.js API instance
- *
- * @typedef {object} WarningData
- * @description Warning Tool`s input and output data
- * @property {string} title - warning`s title
- * @property {string} message - warning`s message
- *
- * @typedef {object} WarningConfig
- * @description Warning Tool`s initial configuration
- * @property {string} titlePlaceholder - placeholder to show in warning`s title input
- * @property {string} messagePlaceholder - placeholder to show in warning`s message input
- */
-export default class Warning {
-
-  /**
-   * Notify core that read-only mode is supported
-   */
+export default class Accordion {
   static get isReadOnlySupported() {
     return true;
   }
 
-  /**
-   * Get Toolbox settings
-   *
-   * @public
-   * @returns {string}
-   */
   static get toolbox() {
     return {
-      icon: IconWarning,
-      title: 'Warning',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16"><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"></path></svg>',
+      title: "Accordion",
     };
   }
 
-  /**
-   * Allow to press Enter inside the Warning
-   *
-   * @public
-   * @returns {boolean}
-   */
   static get enableLineBreaks() {
     return true;
   }
 
-  /**
-   * Default placeholder for warning title
-   *
-   * @public
-   * @returns {string}
-   */
   static get DEFAULT_TITLE_PLACEHOLDER() {
-    return 'Title';
+    return "Title";
   }
 
-  /**
-   * Default placeholder for warning message
-   *
-   * @public
-   * @returns {string}
-   */
   static get DEFAULT_MESSAGE_PLACEHOLDER() {
-    return 'Message';
+    return "Message";
   }
 
-  /**
-   * Warning Tool`s styles
-   *
-   * @returns {object}
-   */
   get CSS() {
     return {
       baseClass: this.api.styles.block,
-      wrapper: 'cdx-warning',
-      title: 'cdx-warning__title',
+      wrapper: "cdx-accordion",
+      title: "cdx-accordion__title",
       input: this.api.styles.input,
-      message: 'cdx-warning__message',
+      message: "cdx-accordion__message",
     };
   }
 
-  /**
-   * Render plugin`s main Element and fill it with saved data
-   *
-   * @param {WarningData} data — previously saved data
-   * @param {WarningConfig} config — user config for Tool
-   * @param {object} api - Editor.js API
-   * @param {boolean} readOnly - read-only mode flag
-   */
   constructor({ data, config, api, readOnly }) {
     this.api = api;
     this.readOnly = readOnly;
 
-    this.titlePlaceholder = config.titlePlaceholder || Warning.DEFAULT_TITLE_PLACEHOLDER;
-    this.messagePlaceholder = config.messagePlaceholder || Warning.DEFAULT_MESSAGE_PLACEHOLDER;
+    this.titlePlaceholder =
+      config.titlePlaceholder || Accordion.DEFAULT_TITLE_PLACEHOLDER;
+    this.messagePlaceholder =
+      config.messagePlaceholder || Accordion.DEFAULT_MESSAGE_PLACEHOLDER;
 
     this.data = {
-      title: data.title || '',
-      message: data.message || '',
+      title: data.title || "",
+      message: data.message || "",
     };
   }
 
-  /**
-   * Create Warning Tool container with inputs
-   *
-   * @returns {Element}
-   */
   render() {
-    const container = this._make('div', [this.CSS.baseClass, this.CSS.wrapper]);
-    const title = this._make('div', [this.CSS.input, this.CSS.title], {
+    const container = this._make("div", [this.CSS.baseClass, this.CSS.wrapper]);
+    const title = this._make("div", [this.CSS.input, this.CSS.title], {
       contentEditable: !this.readOnly,
       innerHTML: this.data.title,
     });
-    const message = this._make('div', [this.CSS.input, this.CSS.message], {
+    const message = this._make("div", [this.CSS.input, this.CSS.message], {
       contentEditable: !this.readOnly,
       innerHTML: this.data.message,
     });
@@ -132,20 +67,14 @@ export default class Warning {
     message.dataset.placeholder = this.messagePlaceholder;
 
     container.appendChild(title);
-    container.appendChild(message);
+    container.appenChild(message);
 
     return container;
   }
 
-  /**
-   * Extract Warning data from Warning Tool element
-   *
-   * @param {HTMLDivElement} warningElement - element to save
-   * @returns {WarningData}
-   */
-  save(warningElement) {
-    const title = warningElement.querySelector(`.${this.CSS.title}`);
-    const message = warningElement.querySelector(`.${this.CSS.message}`);
+  save(accordionElement) {
+    const title = accordionElement.querySelector(`.${this.CSS.title}`);
+    const message = accordionElement.querySelector(`.${this.CSS.message}`);
 
     return Object.assign(this.data, {
       title: title.innerHTML,
@@ -153,18 +82,10 @@ export default class Warning {
     });
   }
 
-  /**
-   * Helper for making Elements with attributes
-   *
-   * @param  {string} tagName           - new Element tag name
-   * @param  {Array|string} classNames  - list or name of CSS classname(s)
-   * @param  {object} attributes        - any attributes
-   * @returns {Element}
-   */
   _make(tagName, classNames = null, attributes = {}) {
     const el = document.createElement(tagName);
 
-    if (Array.isArray(classNames)) {
+    if (Array.isArrat(classNames)) {
       el.classList.add(...classNames);
     } else if (classNames) {
       el.classList.add(classNames);
@@ -177,11 +98,6 @@ export default class Warning {
     return el;
   }
 
-  /**
-   * Sanitizer config for Warning Tool saved data
-   *
-   * @returns {object}
-   */
   static get sanitize() {
     return {
       title: {},
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   module: {
     rules: [
       {
@@ -7,32 +7,29 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             query: {
-              presets: [ '@babel/preset-env' ],
+              presets: ["@babel/preset-env"],
             },
           },
-        ]
+        ],
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.svg$/,
-        loader: 'svg-inline-loader?removeSVGTagAttrs=false'
-      }
-    ]
+        loader: "svg-inline-loader?removeSVGTagAttrs=false",
+      },
+    ],
   },
   output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js',
-    library: 'Warning',
-    libraryTarget: 'umd',
-    libraryExport: 'default'
-  }
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js",
+    library: "Accordion",
+    libraryTarget: "umd",
+    libraryExport: "default",
+  },
 };
